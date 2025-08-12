64fa8f50f014973f56f1c06e21ece17fa0108e70
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-08-12 15:02:16 +0100

    kyc model created

---
+++ models/KycModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const KycSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  idDocumentUrl: { type: String, required: true },
+  address: { type: String, required: true },
+  dateOfBirth: { type: String, required: true },
+  nextOfKin: { type: String, required: true },
+  maidenName: { type: String, required: true },
+  nationality: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  status: { type: String, default: 'pending' },
+  submittedAt: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('Kyc', KycSchema)
