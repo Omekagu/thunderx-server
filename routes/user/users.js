24834656d270fdff28abcdca369f2fcb94c148ee
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-08-27 12:33:47 +0100

    updating users updated

--- routes/user/users.js
+++ routes/user/users.js
@@ -1,5 +1,9 @@
 import express from 'express'
-import { Users } from '../../controllers/user/users.js'
+import {
+  updateUser,
+  updateWalletBalance,
+  Users
+} from '../../controllers/user/users.js'
 import getUserWithWallets from '../../controllers/user/getUserWithWallets.js'
 import { getReferrals } from '../../controllers/auth/referal.js'
 import {
@@ -14,6 +18,8 @@ const router = express.Router()
 router.get('/', Users)
 router.get('/:userId', getUserWithWallets)
 router.get('/referrals/:code', getReferrals)
+router.put('/:userId', updateUser) // <-- update user info
+router.put('/:userId/wallet/:walletId', updateWalletBalance) // <-- update wallet balance
 router.post('/kyc/submit', submitKyc)
 router.get('/kyc/:userId', getUserKyc)
 router.get('/kyc/all/all', getAllUserKycs)
