66b7e9df18ddfb438de6bbb5ced1281abc304459
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-09-02 11:42:59 +0100

    bank gateways created

---
+++ models/BankGateway.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const GatewaySchema = new mongoose.Schema(
+  {
+    provider: { type: String, required: true },
+    country: { type: String, required: true },
+    publicKey: { type: String, required: true },
+    secretKey: { type: String, required: true },
+    callbackUrl: { type: String, required: true },
+    isActive: { type: Boolean, default: true }
+  },
+  { timestamps: true }
+)
+
+const BankGateways =
+  mongoose.models.BankGateway || mongoose.model('BankGateway', GatewaySchema)
+
+export default BankGateways
--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -3,10 +3,20 @@ import {
   getSettings,
   updateSettings
 } from '../../controllers/admin/settings.js'
+import {
+  createBanGateway,
+  deletebankgateway,
+  getBankGateways,
+  updateBankGateway
+} from '../../controllers/admin/BankGateways.js'
 
 const router = express.Router()
 
 router.get('/settings', getSettings)
 router.post('/settings', updateSettings)
+router.get('/bankgateways', getBankGateways)
+router.post('/bankgateways', createBanGateway)
+router.put('/updatebankgateway/:id', updateBankGateway)
+router.delete('/deletebankgateway/:id', deletebankgateway)
 
 export default router
